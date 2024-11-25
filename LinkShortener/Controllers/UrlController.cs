e4fce0bbf00218651dee5f0a48ccb67cfbeb78a8
Talita Dias <talita.dias@itriad.org.br>
2024-11-25 09:51:29 -0400

    first commit

---
+++ LinkShortener/Controllers/UrlController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkShortener.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UrlController : ControllerBase
+    {
+        public UrlController()
+        {
+        }
+
+        [HttpPost("Shortenlink")]
+        public ActionResult<string> ShortenLink(string longUrl)
+        {
+            try
+            {
+                string baseUrl = "talita@estagiaria/";
+
+                string guid = Guid.NewGuid().ToString().Substring(0, 1);
+                string shortenUrl = baseUrl + guid;
+
+                return Ok(shortenUrl);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+            }
+        }
+    }
+}

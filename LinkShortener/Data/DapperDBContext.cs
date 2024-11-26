72e459488957f4095b0abad69656100391d6bea8
Talita Dias <talita.dias@itriad.org.br>
2024-11-26 14:16:35 -0400

    feat:add database connection

---
+++ LinkShortener/Data/DapperDBContext.cs
@@ -0,0 +1,23 @@
+﻿namespace LinkShortener.Data
+{
+    using Dapper;
+    using System.Data.SQLite;
+    using System.Security.Policy;
+
+    public class DapperDBContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperDBContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SQLiteConnection");
+        }
+
+        public SQLiteConnection CreateConnection()
+        {
+            return new SQLiteConnection(_connectionString);
+        }
+    }
+}
--- LinkShortener/Program.cs
+++ LinkShortener/Program.cs
@@ -1,9 +1,13 @@
+using LinkShortener.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<DapperDBContext>();
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddMemoryCache();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
